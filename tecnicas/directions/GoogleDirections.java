45ea7965991950b8d9c5bbd4d1fed6193bed9d89
Geraldo Braz <eng.geraldo.braz@gmail.com>
2014-05-18 16:12:46 -0300

    primeiro commit

---
+++ tecnicas/directions/GoogleDirections.java
@@ -0,0 +1,109 @@
+package tecnicas.directions;
+
+import java.io.*;
+import java.net.*;
+import org.json.*;
+
+//Autor: Geraldo Braz Duarte Filho
+//Modificado em: 18/05/2014
+
+public class GoogleDirections{
+	
+	private URL url;
+	private String googleOutput;
+	
+	/**Método que solicita ao google o arquivo Json com todas as informações 
+	 * de direções. Retorna uma String com o texto do arquivo Json.*/
+	public String solicitacao(String origem, String destino){
+		String googleJson = null;
+		
+		try {
+			url = new URL("http://maps.googleapis.com/maps/api/directions/json?origin=" + 
+											origem + "&destination=" + destino +"&sensor=false");
+			
+			//Abrindo conexão e recebendo o stream direto em forma de String
+			InputStream is = url.openStream();
+			InputStreamReader isr = new InputStreamReader(is);
+			BufferedReader br = new BufferedReader(isr);
+			
+			googleOutput =  br.readLine();
+			
+			//Criando uma string do arquivo Json recebido pelo Google 
+			while(googleOutput != null){
+				if(googleJson == null)
+					googleJson = googleOutput + "\n";
+				else
+					googleJson = googleJson + googleOutput + "\n";
+				googleOutput =  br.readLine();
+			}	
+			br.close();
+			
+		} catch (MalformedURLException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		return googleJson;
+		
+	}
+	
+	/**Método que dá o caminho através de coordenadas geográficas*/
+	public String caminho(String json){
+		String caminhoCoordenadas = "";
+	
+		JSONObject jsonObj = new JSONObject(json);
+		
+		JSONArray routes = jsonObj.getJSONArray("routes");
+		
+		JSONObject routesPrincipal = routes.getJSONObject(0);
+		
+		JSONArray legs = routesPrincipal.getJSONArray("legs");
+		
+		JSONObject legsPrincipal = legs.getJSONObject(0);
+		
+		JSONArray steps = legsPrincipal.getJSONArray("steps");
+			
+		JSONObject stepsPrincipal;
+		
+		for(int i = 0; i < steps.length(); i++){
+			JSONObject jobj4 = steps.getJSONObject(i);
+			
+			//Formatando a string com as coordenadas para uso no link
+			if(i == 0){
+				stepsPrincipal = jobj4.getJSONObject("start_location");
+				caminhoCoordenadas = "" + stepsPrincipal.getDouble("lat");
+				caminhoCoordenadas = caminhoCoordenadas + "," + stepsPrincipal.getDouble("lng");
+				stepsPrincipal = jobj4.getJSONObject("end_location");
+				caminhoCoordenadas = caminhoCoordenadas + "|" + stepsPrincipal.getDouble("lat");
+				caminhoCoordenadas = caminhoCoordenadas + "," + stepsPrincipal.getDouble("lng");
+			}
+			else{
+				stepsPrincipal = jobj4.getJSONObject("end_location");
+				caminhoCoordenadas = caminhoCoordenadas + "|" + stepsPrincipal.getDouble("lat");
+				caminhoCoordenadas = caminhoCoordenadas + "," + stepsPrincipal.getDouble("lng");
+			}
+		}
+		return caminhoCoordenadas;
+	}
+	
+	/**Método que dá o caminho em código polyline*/
+	public String polyline(String json){
+		String polylineStr = "";
+		
+		JSONObject jsonObj = new JSONObject(json);
+		
+		JSONArray routes = jsonObj.getJSONArray("routes");
+		
+		JSONObject routesPrincipal = routes.getJSONObject(0);
+		
+		JSONObject overviewPolyline = routesPrincipal.getJSONObject("overview_polyline");
+		
+		polylineStr = overviewPolyline.getString("points");
+
+		return polylineStr;
+	}
+}
+
