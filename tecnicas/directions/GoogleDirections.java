176fe009822662aef5df62a846058b65dcf123e4
Geraldo Braz <eng.geraldo.braz@gmail.com>
2014-05-18 17:01:52 -0300

    commit 2

--- tecnicas/directions/GoogleDirections.java
+++ tecnicas/directions/GoogleDirections.java
@@ -8,14 +8,14 @@ import org.json.*;
 //Modificado em: 18/05/2014
 
 public class GoogleDirections{
-	
-	private URL url;
-	private String googleOutput;
-	
 	/**Método que solicita ao google o arquivo Json com todas as informações 
-	 * de direções. Retorna uma String com o texto do arquivo Json.*/
-	public String solicitacao(String origem, String destino){
+	 * de direções. Retorna uma String com o texto do arquivo Json.
+	 * @param String origem, String destino
+	 * @return String json*/
+	public static String solicitacao(String origem, String destino){
 		String googleJson = null;
+		String googleOutput;
+		URL url;
 		
 		try {
 			url = new URL("http://maps.googleapis.com/maps/api/directions/json?origin=" + 
@@ -50,8 +50,10 @@ public class GoogleDirections{
 		
 	}
 	
-	/**Método que dá o caminho através de coordenadas geográficas*/
-	public String caminho(String json){
+	/**Método que dá o caminho através de coordenadas geográficas
+	 * @param String json
+	 * @return String caminhoCoordenadas*/
+	public static String caminho(String json){
 		String caminhoCoordenadas = "";
 	
 		JSONObject jsonObj = new JSONObject(json);
@@ -89,8 +91,10 @@ public class GoogleDirections{
 		return caminhoCoordenadas;
 	}
 	
-	/**Método que dá o caminho em código polyline*/
-	public String polyline(String json){
+	/**Método que dá o caminho em código polyline
+	 * @param String json
+	 * @return String polyline*/
+	public static String polyline(String json){
 		String polylineStr = "";
 		
 		JSONObject jsonObj = new JSONObject(json);
